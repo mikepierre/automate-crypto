6ceaff29ea3f6fdd6fd67b9f8eeda0654ab31bf5
mikepierre <mikepierredotcom@gmail.com>
2018-05-02 14:37:42 -0400

    Added jQuery text manipulation

--- app/static/js/ajax.js
+++ app/static/js/ajax.js
@@ -1,7 +1,22 @@
 function getCoinData(){
 	$.get( "/get-currency?ico=rcn", function( data ) {
-	  console.log( data);
-	  console.log(data[0].Ask);
+	  //console.log( data);
+	  //console.log(data.result[0].Ask);
+	  $( document ).ready(function() {
+		  $("#ask").text(data.result[0].Ask);
+		  $("#baseVolume").text(data.result[0].BaseVolume);
+		  $("#bid").text(data.result[0].Bid);
+		  $("#created").text(data.result[0].Created);
+		  $("#high").text(data.result[0].High);
+		  $("#last").text(data.result[0].Last);
+		  $("#low").text(data.result[0].Low);
+		  $("#marketName").text(data.result[0].MarketName);
+		  $("#openBuyOrders").text(data.result[0].OpenBuyOrders);
+		  $("#openSellOrders").text(data.result[0].OpenSellOrders);
+		  $("#prevDate").text(data.result[0].PrevDay);
+		  $("#timeStamp").text(data.result[0].TimeStamp);
+		  $("#volume").text(data.result[0].Volume);
+	  });
 	});
 }
 
